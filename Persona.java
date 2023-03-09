7d4d77248490cc5f17de52b997a2625aced77865
Juanpablolascoaga <122291722+Juanpablolascoaga@users.noreply.github.com>
2023-03-08 20:30:41 -0600

    Delete Persona.java

--- Persona.java
+++
@@ -1,8 +0,0 @@
-package EjerciciosAbstraccion;
-
-public class Persona extends Humano {
-	
-	public Persona(String nombre, boolean trabaja, boolean estudia, boolean ejercita, boolean viaja) {
-		super(nombre,trabaja,estudia,ejercita,viaja);
-}
-}
