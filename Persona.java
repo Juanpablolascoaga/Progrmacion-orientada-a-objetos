5aeeeb352105106d90fa9f8a46b9285a97c623b4
Juanpablolascoaga <122291722+Juanpablolascoaga@users.noreply.github.com>
2023-03-08 20:31:18 -0600

    Add files via upload

---
+++ Persona.java
@@ -0,0 +1,8 @@
+package EjerciciosAbstraccion;
+
+public class Persona extends Humano {
+	
+	public Persona(String nombre, boolean trabaja, boolean estudia, boolean ejercita, boolean viaja) {
+		super(nombre,trabaja,estudia,ejercita,viaja);
+}
+}
